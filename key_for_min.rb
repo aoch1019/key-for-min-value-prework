72ee1605f8b7490e4bfd1284f396be10ffa6f1a4
Andrew Och <andrewoch95@gmail.com>
2018-07-10 21:43:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if lowest_value == nil || value < lowest_value
       lowest_key = key
+      lowest_value = value
     end
   end
 
